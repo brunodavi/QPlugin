5ec202b1d08f369e7a242c9b2930f1aa1c4dff8b
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2020-11-13 03:02:33 -0300

    add database.db + actions

---
+++ QPlugin/example.py
@@ -0,0 +1,5 @@
+from qplugin.tasks import Alert
+
+alert = Alert()
+alert.Toast('Testando...')
+alert.Beep(duration=300)
---
+++ QPlugin/qplugin/__init__.py
@@ -0,0 +1,9 @@
+from .utils import *
+
+APP = 'QPlugin'
+LOCAL = f'/sdcard/{APP}'
+
+LOG = f'{LOCAL}/log'
+RUNLOG = f'{LOG}/runlog.txt'
+
+DATABASE = f'{LOCAL}/database.db'
